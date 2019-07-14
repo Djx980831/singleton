012d86bcaa42bd6ef2a415ad36dad132b4d4f1b6
DJX <apple@appledeMacBook-Pro.local>
2019-07-14 18:01:56 +0800

    hungry singleton

--- src/singleton/HungrySongleton.java
+++ src/singleton/HungrySongleton.java
@@ -1,5 +1,14 @@
 package singleton;
 
 public class HungrySongleton {
-    
+
+    private HungrySongleton() {
+    }
+
+    public static HungrySongleton single = new HungrySongleton();
+
+    public static HungrySongleton getInstance(){
+        return single;
+    }
+
 }
